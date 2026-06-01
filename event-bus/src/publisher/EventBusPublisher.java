fd3b655394c2e173245baa20e7ea5c617798d3b0
53038 <53038@students.riphah.edu.pk>
2026-06-01 21:23:11 +0500

    Student C - implemented EventBusPublisher

--- event-bus/src/publisher/EventBusPublisher.java
+++ event-bus/src/publisher/EventBusPublisher.java
@@ -1,1 +1,29 @@
+package com.sdapro.eventbus.publisher;
 
+// PATTERN: Singleton
+// PATTERN: Observer
+
+public class EventBusPublisher {
+
+    private static EventBusPublisher instance;
+
+    private EventBusPublisher() {
+    }
+
+    public static synchronized EventBusPublisher getInstance() {
+
+        if (instance == null) {
+            instance = new EventBusPublisher();
+        }
+
+        return instance;
+    }
+
+    public void publish(String eventType, Object event) {
+
+        System.out.println(
+            "Publishing event: " + eventType
+        );
+
+    }
+}
